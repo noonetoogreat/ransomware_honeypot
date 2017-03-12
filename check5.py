801ef1bc028ec4ea1c56ffe1afb4222d3a886de2
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2017-03-11 19:01:36 -0500

    test 17

--- check5.py
+++ check5.py
@@ -7,7 +7,7 @@ import volatility.addrspace as addrspace
 registry.register_global_options(config, commands.Command)
 registry.register_global_options(config, addrspace.BaseAddressSpace)
 config.parse_options()
-config.PROFILE="Win7SP0x64"
+config.PROFILE="Win7SP0x86"
 config.LOCATION = "file:///media/memory/private/image.dmp"
 import volatility.plugins.taskmods as taskmods
 p = taskmods.PSList(config)
