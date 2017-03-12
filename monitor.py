edfc7ee8c2812231ab36f71d7c300396dbb25d36
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2017-03-11 21:31:50 -0500

    test 23

--- monitor.py
+++ monitor.py
@@ -21,6 +21,7 @@ def monitor(regex):
 				for files in proci.open_files() :
 					#print files.path
 					#handles = re.match(my_regex, files, re.IGNORECASE)
+					print files.path
 					match = regex.search(str(files))
 					if match is not None:
 						'''
